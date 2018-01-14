39f12ee6005cd59f83257e0efa33e5ebc4a9ed25
vitaly.leontev <vleon2690@gmail.com>
2018-01-14 20:49:10 +0200

    PostFilter and PostProvider beans created

--- src/main/java/ua/dp/skillsup/spring/homework/PostFilter.java
+++ src/main/java/ua/dp/skillsup/spring/homework/PostFilter.java
@@ -5,9 +5,11 @@ import java.util.stream.Collectors;
 
 public class PostFilter {
 
+
     public List<Post> filterByKeyword(List<Post> posts, String keyWord){
         return posts.stream().filter((Post post) ->
                 post.getText().toLowerCase().contains(keyWord.toLowerCase()))
                 .collect(Collectors.<Post>toList());
     }
+
 }
